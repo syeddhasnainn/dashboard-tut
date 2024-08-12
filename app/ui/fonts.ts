f2c5f86372d620a76e4e77b361358ad88d163988
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-08-12 17:17:20 +0100

    fixed font name

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-export const lusitna = Lusitana({subsets:['latin'], weight: '400'})
+export const lusitana = Lusitana({subsets:['latin'], weight: '400'})
 export const inter = Inter({ subsets: ['latin'] });
